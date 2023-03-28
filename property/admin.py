b08ac03857aa907070fc43a57cc0b0e75284c8a8
SGKespace <55636018+SGKespace@users.noreply.github.com>
2023-03-28 19:50:02 +0400

    Update admin.py

--- property/admin.py
+++ property/admin.py
@@ -5,4 +5,11 @@ class AuthorAdmin(admin.ModelAdmin):
     search_fields = ('owner', 'town', 'address',)
     readonly_fields = ('created_at',)
 
+    list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
+    list_editable = ( 'price', 'new_building', 'construction_year', 'town')
+
+    def __str__(self):
+        return self.address
+
+
 admin.site.register(Flat, AuthorAdmin)
