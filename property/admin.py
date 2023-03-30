f6c694ce96dd32dd0651a23595881082584101fc
SGKespace <55636018+SGKespace@users.noreply.github.com>
2023-03-30 17:15:25 +0400

    Django: инлайны

--- property/admin.py
+++ property/admin.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
+
 from .models import Flat, Claim, Owner
 
-class AuthorAdmin(admin.ModelAdmin):
-    search_fields = ('owner', 'town', 'address',)
-    readonly_fields = ('created_at',)
 
+class OwnerFlatInline(admin.TabularInline):
+    model = Owner.owned_flats.through
+    raw_id_fields = ("owner", "flat")
+
+
+class FlatAdmin(admin.ModelAdmin):
+    search_fields = ('owner','town', 'address',)
+    readonly_fields = ('created_at',)
     list_display = ('address', 'price', 'new_building', 'construction_year', 'town')
-    list_editable = ('price', 'new_building', 'construction_year', 'town')
+    list_editable = ( 'price', 'new_building', 'construction_year', 'town')
     list_filter = ['new_building']
     raw_id_fields = ("likes",)
+    inlines = [OwnerFlatInline,]
 
     def __str__(self):
         return self.address
@@ -22,6 +29,8 @@ class OwnerAdmin(admin.ModelAdmin):
     raw_id_fields = ['owned_flats']
 
 
-admin.site.register(Flat, AuthorAdmin)
+
+
+admin.site.register(Flat, FlatAdmin)
 admin.site.register(Claim, ClaimAdmin)
 admin.site.register(Owner, OwnerAdmin)
