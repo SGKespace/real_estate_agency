7a2ca632069b0cdfead91ec538825d7da053226f
SGK <55636018+SGKespace@users.noreply.github.com>
2023-03-31 16:20:35 +0400

    Отформатируйте код

--- property/migrations/0003_auto_20230329_1355.py
+++ property/migrations/0003_auto_20230329_1355.py
@@ -4,8 +4,8 @@ from django.db import migrations
 
 def set_new_building(apps, schema_editor):
     Flat = apps.get_model('property', 'Flat')
-    Flat.objects.filter(construction_year__gte = 2015).update(new_building = True)
-    Flat.objects.filter(construction_year__lt = 2015).update(new_building = False)
+    Flat.objects.filter(construction_year__gte=2015).update(new_building=True)
+    Flat.objects.filter(construction_year__lt=2015).update(new_building=False)
 
 
 
